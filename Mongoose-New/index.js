22cc92388e17f2f02d5699eb818fb95392f81b09
Dikshith <dikshithrrddy@gmail.com>
2024-08-11 23:08:16 +0530

    11/8/24

--- Mongoose-New/index.js
+++ Mongoose-New/index.js
@@ -1,8 +1,41 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true , useUnifiedtopology: true});
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/harryKart", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 var db = mongoose.connection;
-db.on('error',console.error.bind(console,'connection error:'));
-db.once('open',function(){
-    console.log("we're connected Dude") //we're connected
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log("we're connected Dude");
+});
+
+var kittySchema = new mongoose.Schema({ //making Schema for Mongoose
+  name: String,
+});
+
+kittySchema.methods.speak = function () {
+    var greeting = "My name is " + this.name;
+    // console.log(greeting);
+};
+
+var Kitten = mongoose.model('newKitty', kittySchema);
+var newKitty = new Kitten({ name: 'newKitty' });
+
+// Use async/await for saving
+async function saveKitty() {
+    try {
+        await newKitty.save();
+        newKitty.speak();
+    } catch (err) {
+        console.error('Error occurred:', err);
+    }
+}
+
+saveKitty();
+
+
+Kitten.find({name: "newKitty"},function(err,kittens) {
+    if (err) return console.error(err);
+    console.log(kittens);
 });
